a187e248d6d6b63bf4b6bb8bb473608582cfa60d
DantheCE <dan.doyin@gmail.com>
2026-02-20 15:54:29 +0100

    feat: Implement initial backend API for notes with Express.js, MongoDB integration, and ESLint configuration.

--- Backend/models/note.js
+++ Backend/models/note.js
@@ -5,49 +5,49 @@ const url = process.env.MONGODB_URL
 
 console.log('connecting to database')
 
-mongoose.connect(url, {family: 4})
-.then(result => {
+mongoose.connect(url, { family: 4 })
+  .then(() => {
     console.log('connected to MongoDB')
-}) 
-.catch(error => {
+  })
+  .catch(error => {
     console.log('error connecting to MongoDB: ', error.message)
-})
+  })
 
 const noteSchema = new mongoose.Schema({
-    content: {
-        type: String,
-        minLength: 5,
-        required: true
-    },
-    important: Boolean,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  important: Boolean,
 })
 
 noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
-noteSchema.pre('update', function(next){
-    this.setOptions({runValidators: true, context: 'query'})
-    next();
+noteSchema.pre('update', function (next) {
+  this.setOptions({ runValidators: true, context: 'query' })
+  next()
 })
-noteSchema.pre('FindOneAndupdate', function(next){
-    this.setOptions({runValidators: true, context: 'query'})
-    next();
+noteSchema.pre('FindOneAndupdate', function (next) {
+  this.setOptions({ runValidators: true, context: 'query' })
+  next()
 })
-noteSchema.pre('FindByIdAndupdate', function(next){
-    this.setOptions({runValidators: true, context: 'query'})
-    next();
+noteSchema.pre('FindByIdAndupdate', function (next) {
+  this.setOptions({ runValidators: true, context: 'query' })
+  next()
 })
-noteSchema.pre('updateOne', function(next){
-    next();
+noteSchema.pre('updateOne', function (next) {
+  next()
 })
-noteSchema.pre('updateMany', function(next){
-    this.setOptions({runValidators: true, context: 'query'})
-    next();
+noteSchema.pre('updateMany', function (next) {
+  this.setOptions({ runValidators: true, context: 'query' })
+  next()
 })
 
 module.exports = mongoose.model('Note', noteSchema)
