5fb23fd3cf89eb4ebeaf7675ebd332e0c53a51c4
DantheCE <dan.doyin@gmail.com>
2026-02-26 17:55:51 +0100

    refactor: enhance user retrieval with populated notes and clean up user model

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -17,28 +17,6 @@ const userSchema = new mongoose.Schema({
   ],
 })
 
-// virtual field to hold the plain-text password temporarily
-userSchema
-  .virtual('password')
-  .set(function (password) {
-    this._password = password
-  })
-  .get(function () {
-    return this._password
-  })
-
-// password validation runs when the document is saved
-userSchema.path('passwordHash').validate(function (value) {
-  // only perform checks if a plain password was provided
-  if (this._password) {
-    if (this._password.length < 3) {
-      this.invalidate('password', 'Password must be at least 3 characters long')
-      return false
-    }
-  }
-  return true
-}, null)
-
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
